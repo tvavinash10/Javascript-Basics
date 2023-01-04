8286049d5b2594f50d311de67de6a9fb180b7454
Avinash.T.V <tvavinash10@gmail.com>
2023-01-04 22:59:39 +0530

    Data Types

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // log statement using console.log
-console.log('Hello from main.js');
-
+// console.log("Hello from main.js");
 
 // variables
 // let age = 25
@@ -12,3 +11,39 @@ console.log('Hello from main.js');
 // let sum =0
 // sum = 5
 // console.log(sum)
+
+//Data Types
+//Primitive Types
+// const name = "avinash";
+// const language = "Javascript";
+// const channel = `Codevolution`;
+
+// const total = 0;
+// const PI = 3.14;
+
+// const isPrimaryNumber = true;
+// const isNewUser = false;
+
+// let result;
+// console.log(result);
+
+// const res = null;
+// const data = null;
+
+//Non Primitive Types
+//Object literals
+// const person = {
+//     firstName: "Bruce",
+//     lastName: "Wayne",
+//     age: 30,
+// };
+
+// console.log(person.firstName)
+
+// const oddNumbers = [1,3,5,7,9]
+// console.log(oddNumbers[2])
+
+// let a = 10
+// a= 'avi'
+// a= true
+// console.log(a)

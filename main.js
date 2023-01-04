c8b9bcb75b61865caa6c16678a775f657a7570b7
Avinash.T.V <tvavinash10@gmail.com>
2023-01-04 22:19:45 +0530

    Variables

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // log statement using console.log
 console.log('Hello from main.js');
+
+
+// variables
+// let age = 25
+// console.log(age)
+
+// const salary =0
+// salary = 80000
+// console.log(salary)
+// let sum =0
+// sum = 5
+// console.log(sum)
